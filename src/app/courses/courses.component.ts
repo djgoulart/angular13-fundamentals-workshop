837dd30fe1f8a7a52ce756bdef4758e23d375187
Diego Goulart <diegogoulart.aws@gmail.com>
2022-11-20 23:20:09 -0300

    feat: implement a form to edit a course

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { Course } from '../common/models/course';
+
+const emptyCourse: Course = {
+  id: null,
+  title: '',
+  description: '',
+  percentComplete: 0,
+  favorite: false
+}
 
 @Component({
   selector: 'app-courses',
@@ -6,24 +15,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./courses.component.scss']
 })
 export class CoursesComponent implements OnInit {
-  courses = [
+  selectedCourse = emptyCourse;
+  originalTitle: string;
+
+  courses: Course[] = [
     {
-      id: 1,
+      id: '1',
       title: 'Angular 13 Fundamentals',
       description: 'Learn the fundamentals of Angular 13',
       percentComplete: 26,
       favorite: true
     },
     {
-      id: 2,
+      id: '2',
       title: 'React Fundamentals',
       description: 'Learn the fundamentals of React',
       percentComplete: 95,
       favorite: true
     },
   ];
-
-  selectedCourse = null;
 
   constructor() { }
 
@@ -32,17 +42,24 @@ export class CoursesComponent implements OnInit {
 
   selectCourse(course) {
     if(course) {
-      this.selectedCourse = course;
+      this.selectedCourse = {...course};
+      this.originalTitle = course.title;
     }
   }
 
   deleteCourse(courseId) {
     if(courseId) {
-
+      console.log('DELETE COURSE', courseId)
     }
   }
 
-  clearSelect(){
-    this.selectedCourse = null;
+  reset(){
+    this.selectCourse({...emptyCourse});
+  }
+
+  saveCourse(course){
+    if(course) {
+      console.log('SAVED COURSE', course)
+    }
   }
 }
